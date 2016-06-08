412e87ad2740505b37ea56fb362a07234306e413
carlosgim <scire1905@gmail.com>
2016-06-07 21:28:54 -0300

    msens

---
+++ Franco.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Create by Carlos A. Gimenez
+"""
+
+import os
+         
+print '+++ Push all my stuff +++'
+
+os.system('git add -A')
+
+msens = raw_input('Please write a nice message here: ')
+
+mensaje = 'git commit -m '+ "'msens'"
+
+os.system(mensaje)
+
+push = 'git push origin '+ Franco
+
+os.system(push)
+
+print 'Have fun and enjoy your meal...'
