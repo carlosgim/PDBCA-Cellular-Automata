92b38933ed6820662d26c5e06b23da4b2f5ad2ed
santiagowiedmann <santiagowiedmann@gmail.com>
2016-06-08 22:04:06 -0300

    Sketch of the rules module

--- src/tg_kernel/src/modules/mod_rules.f90
+++ src/tg_kernel/src/modules/mod_rules.f90
@@ -1,69 +1,184 @@
-!--------------------------------------------------------------------------
-!REFERENCIA
+!=============================================================================
+!This module computes a set of rules for the cellular automata. Till the moment
+!is just an sketch
 ! 
-!---------------------------------------------------------------------------
-!DATOS
-!---------------------------------------------------------------------------
-program tg_kernel
+!=============================================================================
+! Name: Santiago Walterio, Wiedmann
+! Date: 
+!=============================================================================
+program mod_rules
 
 implicit none
-integer, parameter :: dp = selected_real_kind(15,307)
-!real(kind=dp), allocatable :: x(:), x_old(:)
-character, allocatable :: x(:,:), x_old(:,:)
-integer :: i, j, k, step_num, n
+real :: prob_m, prob_ij, prob_0, acid_0, acid_ij, grwth_0, grwth_ij, 
+integer :: i, j, l, m, k, t, n, step_num,
+real, dimension(:,:,4) :: state
+real, dimension(:,:) :: temp
 
-! Set initial parameters
-n = 80; step_num = 50
+!Set initial parameters
+print*, "introduce the state matrix dimension, please "
+read*, n
+print*, "introduce the step number, please "
+read*, step_num
+allocate (state(n,n,4))
+allocate (temp(n,n))
 
-! Allocate the size of the array
-allocate( x(0:n+1, 1)); allocate( x_old(0:n+1, 1))
+do l = 1,n
 
-x(0:n+1, 1) = ' '
-x(40, 1)    = '*'
+    do m = 1,n
 
-step_numbers: do i = 1, step_num
+        temp(l,m) = 0
 
-    x_old(0:n+1, 1) = x(0:n+1, 1)
+    end do
 
-    width_array: do j = 1, n
+end do
 
-        long_array: do k = 1, 1
+!Set the rules
+step_numbers: do t = 1, step_num
 
-            if (( x_old(j-1, k) == ' ' .and. &
-                  x_old(j, k)   == ' ' .and. &
-                  x_old(j+1, k) == '*' )     &
-                  .or. &
-                ( x_old(j-1, k) == '*' .and.  &
-                  x_old(j, k)   == ' ' .and.  &
-                  x_old(j+1, k) == ' ' )      &
-                  .or. &
-                ( x_old(j-1, k) == ' ' .and.  &
-                 x_old(j, k)   == '*' .and.  &
-                 x_old(j+1, k) == ' ' )      &
-                  .or. &
-               ( x_old(j-1, k) == '*' .and.  &
-                 x_old(j, k)  ==  ' ' .and.   &
-                 x_old(j+1, k) == ' ' ) ) then
-                 x(j, k) = '*'
-            else
-                 x(j, k) = ' '
-            end if
+    k = 0
 
-        end do long_array
+    while k <= n**2 then
 
-    end do width_array
+        x_rnd = random(1:n)
+        y_rnd = random(1:n)
+        x_rnd = i
+        y_rnd = j
 
-! Boundary
-    
-    x(0, k) = x(n, k); x(n+1, k) = x(1, k)
-    
-! Print
-    write(*,*) 'step', step_num
-    write(*,'(80a)') x(1:n, k)              
-    
-end do step_numbers
+        if ( temp(i,j) == 0 ) then
+            temp(i,j) = 1
+            k = k + 1
+!========================================================================================
+!                                      LIVING CELLS
+!========================================================================================
 
-      stop
-end program tg_kernel
-     
+	    if      ( state(i,j,4) == 2       .and. &
+       	              acid_ij <= acid_0 )      then
+                      state(i,j,4) = 0
+                      state(i,j,3) = grwth_0
+
+            else if ( state(i,j,4) == 2       .and. &
+                    ( state(i-1,j-1,4) == 1    .or. &
+                      state(i,j-1,4) == 1      .or. &
+                      state(i+1,j-1,4) == 1    .or. &
+                      state(i-1,j,4) == 1      .or. &
+                      state(i+1,j,4) == 1      .or. &
+                      state(i,j+1,4) == 1      .or. &
+                      state(i-1,j+1,4) == 1    .or. &
+                      state(i,j+1,4) == 1      .or. &
+                      state(i+1,j+1,4) == 1 ) .and. &
+                    ( acid_ij >= acid_0 )     .and. &
+                    ( prob_0 <= prob_ij )      then
+                      state(i,j,4) = 3
+                      state(i,j,3) = grwth_0
+
+             else if ( state(i,j,4) == 3       .and. &
+                     ( state(i-1,j-1,4) == 1    .or. &
+                       state(i,j-1,4) == 1      .or. &
+                       state(i+1,j-1,4) == 1    .or. &
+                       state(i-1,j,4) == 1      .or. &
+                       state(i+1,j,4) == 1      .or. &
+                       state(i,j+1,4) == 1      .or. &
+                       state(i-1,j+1,4) == 1    .or. &
+                       state(i,j+1,4) == 1      .or. &
+                       state(i+1,j+1,4) == 1 ) .and. &
+                     ( acid_ij >= acid_0 )     .and. &
+                     ( prob_0 <= prob_ij )      then
+                       state(i,j,4) = 4
+                       state(i,j,3) = grwth_0
+
+              else if ( state(i,j,4) == 4       .and. &
+                      ( state(i-1,j-1,4) == 1    .or. &
+                        state(i,j-1,4) == 1      .or. &
+                        state(i+1,j-1,4) == 1    .or. &
+                        state(i-1,j,4) == 1      .or. &
+                        state(i+1,j,4) == 1      .or. &
+                        state(i,j+1,4) == 1      .or. &
+                        state(i-1,j+1,4) == 1    .or. &
+                        state(i,j+1,4) == 1      .or. &
+                        state(i+1,j+1,4) == 1 ) .and. &
+                      ( acid_ij >= acid_0 )     .and. &
+                      ( prob_0 <= prob_ij )      then
+                        state(i,j,4) = 5
+                        state(i,j,3) = grwth_0
+
+              else if ( state(i,j,4) == 5       .and. &
+                        state(i-1,j-1,4) == 1 )  then
+                        state(i,j,4) = 2
+                        state(i-1,j-1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i-1,j-1,3) = grwth_0 
   
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i,j-1,4) == 1 )     then
+                        state(i,j,4) = 2
+                        state(i,j-1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i,j-1,3) = grwth_0
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i+1,j-1,4) == 1 )   then
+                        state(i,j,4) = 2
+                        state(i+1,j-1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i+1,j-1,3) = grwth_0 
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i-1,j,4) == 1 )     then
+                        state(i,j,4) = 2
+                        state(i-1,j,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i-1,j,3) = grwth_0
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i+1,j,4) == 1 )     then
+                        state(i,j,4) = 2
+                        state(i+1,j,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i+1,j,3) = grwth_0
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i-1,j+1,4) == 1 )   then
+                        state(i,j,4) = 2
+                        state(i-1,j+1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i-1,j+1,3) = grwth_0
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i,j+1,4) == 1 )     then
+                        state(i,j,4) = 2
+                        state(i,j+1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i,j+1,3) = grwth_0
+
+               else if ( state(i,j,4) == 5       .and. &
+                        state(i+1,j+1,4) == 1 )     then
+                        state(i,j,4) = 2
+                        state(i+1,j+1,4) = 2
+                        state(i,j,3) = grwth_0
+                        state(i+1,j+1,3) = grwth_0
+
+                 
+              else 
+                        state(i,j,4) = 2
+                        state(i,j,3) = grwth_0
+!========================================================================================
+!                                      CANCER CELLS
+!========================================================================================
+
+
+
+
+
+!========================================================================================
+!                                      
+!========================================================================================
+              else 
+
+                  k = k
+
+              end if
+
+
+
+end do
+     
